e506fd6583f51718c7c8247e4d9251068add9119
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2025-03-28 14:28:08 +0900

    Update 4153.kt

--- BaekjoonOnlineJudge/4153/4153.kt
+++ BaekjoonOnlineJudge/4153/4153.kt
@@ -1,12 +1,14 @@
 fun main() {
-    do {
-        val input = readln().split(" ").map { it.toInt() }.sortedDescending()
+    while(true){
+        val input = readln().split(" ").map { it.toInt() }.toMutableList()
         if(input.sum() == 0) break
+
+        input.sort()
 
         val a = input[0] * input[0]
         val b = input[1] * input[1]
         val c = input[2] * input[2]
 
-        println(if (a == b + c) "right" else "wrong")
-    } while (true)
+        println(if (a + b == c) "right" else "wrong")
+    }
 }
