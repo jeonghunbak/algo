f64e572b0e115ff00f93f0fae3c74da9b57d080c
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2025-04-07 09:39:56 +0900

    Update 10814.kt

--- BaekjoonOnlineJudge/10814/10814.kt
+++ BaekjoonOnlineJudge/10814/10814.kt
@@ -2,16 +2,14 @@ data class Person(val name: String, val age: Int, val joinNum: Int)
 
 fun main() {
     val cnt = readln().toInt()
-    val arr: Array<Person?> = arrayOfNulls(cnt)
+    val arr = mutableListOf<Person>()
 
     repeat(cnt){ index ->
         val input = readln().split(" ")
-        arr[index] = Person(input[1], input[0].toInt(), index)
+        arr.add(Person(input[1], input[0].toInt(), index))
     }
 
-    val sortedArr = arr.sortedArrayWith(compareBy<Person?> { it?.age }.thenBy { it?.joinNum })
+    arr.sortWith(compareBy( { it.age }, { it.joinNum } ))
 
-    sortedArr.forEach { person ->
-        println("${person?.age} ${person?.name}")
-    }
+    arr.forEach { println("${it.age} ${it.name}") }
 }
