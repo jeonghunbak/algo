8397ca0835bacb3446e2326431cd4cb8d93c6a0c
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2025-04-07 09:13:56 +0900

    solved with Kotlin

---
+++ BaekjoonOnlineJudge/11650/11650.kt
@@ -0,0 +1,15 @@
+data class Coordinate(val x: Int, val y: Int)
+
+fun main() {
+    val cnt = readln().toInt()
+    val arr = mutableListOf<Coordinate>()
+
+    repeat(cnt){
+        val input = readln().split(" ").map { it.toInt() }
+        arr.add(Coordinate(input[0], input[1]))
+    }
+
+    arr.sortWith(compareBy( { it.x }, { it.y } ))
+
+    arr.forEach { println("${it.x} ${it.y}") }
+}
