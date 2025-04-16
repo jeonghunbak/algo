8632b3bad8ecd513eb6135c5cabaab77039179f3
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2025-04-16 23:49:29 +0900

    Update 120905.kt

--- programmers/120905/120905.kt
+++ programmers/120905/120905.kt
@@ -1,9 +1,11 @@
-fun solution(n: Int, numlist: IntArray): IntArray {
-    val answer = ArrayList<Int>()
-
-    numlist.forEach {
-        if(it % n == 0) answer.add(it)
+class Solution {
+    fun solution(n: Int, numlist: IntArray): IntArray {
+        val answer = ArrayList<Int>()
+    
+        numlist.forEach {
+            if(it % n == 0) answer.add(it)
+        }
+    
+        return answer.toIntArray()
     }
-
-    return answer.toIntArray()
 }
