228666b30c21c6077be1377f592605ba9a588e1d
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2025-08-30 19:42:24 +0900

    Create 27889.kt

---
+++ BaekjoonOnlineJudge/27889/27889.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val map = mapOf(
+        "NLCS" to "North London Collegiate School",
+        "BHA" to "Branksome Hall Asia",
+        "KIS" to "Korea International School",
+        "SJA" to "St. Johnsbury Academy",
+    )
+    print(map.getValue(readln()))
+}
