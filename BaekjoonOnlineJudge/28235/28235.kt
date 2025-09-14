3dabfdba42c55b3b2b0ee36cd48b0338f0785a11
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2025-09-14 10:36:26 +0900

    Create 28235.kt

---
+++ BaekjoonOnlineJudge/28235/28235.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val input = readln()
+
+    when(input) {
+        "SONGDO" -> print("HIGHSCHOOL")
+        "CODE" -> print("MASTER")
+        "2023" -> print("0611")
+        "ALGORITHM" -> print("CONTEST")
+    }
+}
