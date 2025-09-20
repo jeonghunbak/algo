bc6112ebb37cdea9ee1a42cbd0abaed29f7a866a
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2025-09-20 12:47:54 +0900

    Create 28691.kt

---
+++ BaekjoonOnlineJudge/28691/28691.kt
@@ -0,0 +1,9 @@
+fun main(){
+    when(readln()) {
+        "M" -> print("MatKor")
+        "W" -> print("WiCys")
+        "C" -> print("CyKor")
+        "A" -> print("AlKor")
+        "$" -> print("\$clear")
+    }
+}
