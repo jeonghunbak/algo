79e8465c56dab0a80dd73bfa967b3cd2054cd95d
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2026-01-21 08:02:43 +0900

    Create 23235.kt

---
+++ BaekjoonOnlineJudge/23235/23235.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var idx = 1
+    while(true){
+        val input = readln()
+        if(input == "0") break
+        
+        println("Case ${idx}: Sorting... done!")
+        idx++
+    }
+}
