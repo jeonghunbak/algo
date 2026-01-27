0e51f2690d323387d97d4b6c9c68e8fd34f9f0bb
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2026-01-27 12:20:58 +0900

    Create 24309.kt

---
+++ BaekjoonOnlineJudge/24309/24309.kt
@@ -0,0 +1,10 @@
+import java.math.BigInteger
+
+fun main() {
+    val a = BigInteger(readln())
+    val b = BigInteger(readln())
+    val c = BigInteger(readln())
+
+    val x = (b - c) / a
+    println(x)
+}
