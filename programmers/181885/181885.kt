dd555fb49ef4318191b82097b2268139e44c4500
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2026-05-21 08:06:48 +0900

    Create 181885.kt

---
+++ programmers/181885/181885.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> {
+        return todo_list.filterIndexed { index, _ ->
+            !finished[index]
+        }.toTypedArray()
+    }
+}
