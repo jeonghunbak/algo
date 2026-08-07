992b85c7b9ef59904540a6046669fa557b0143c1
Cloud <51371568+jeonghunbak@users.noreply.github.com>
2026-08-07 09:01:09 +0900

    Create 209.kt

---
+++ LeetCode/209/209.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun minSubArrayLen(target: Int, nums: IntArray): Int {
+        var sum = 0
+        var left = 0
+        var minLen = Int.MAX_VALUE
+
+        for (right in nums.indices) {
+            sum += nums[right]
+
+            while (sum >= target) {
+                minLen = minOf(minLen, right - left + 1)
+
+                sum -= nums[left]
+                left++
+            }
+        }
+        return if (minLen == Int.MAX_VALUE) 0 else minLen
+    }
+}
